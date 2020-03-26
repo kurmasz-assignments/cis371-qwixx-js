aa96a51d33ee1b26e3b2f6cad2f25243263606ff
Zachary Kurmas <kurmasz@gvsu.edu>
2020-03-26 17:44:45 -0400

    Fixed an oversight in the controller sample test.

--- test/QwixxController.test.js
+++ test/QwixxController.test.js
@@ -14,6 +14,7 @@ describe("QwixxController", () => {
 
         view = {
             onRollButtonClicked: jest.fn(),
+	    onNumberButtonClicked: jest.fn(),
             update: jest.fn()
         };
 
